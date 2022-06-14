d35e0719329d04c16efd6759815f521486386869
刘元博 <1553592282@qq.com>
2022-06-14 21:31:54 +0800

    添加普通登录模式接口

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysLoginController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysLoginController.java
@@ -52,6 +52,23 @@ public class SysLoginController
     }
 
     /**
+     * 登录方法 - 新
+     *
+     * @param loginBody 登录信息
+     * @return 结果
+     */
+    @PostMapping("/newLogin")
+    public AjaxResult newLogin(LoginBody loginBody)
+    {
+        AjaxResult ajax = AjaxResult.success();
+        // 生成令牌
+        String token = loginService.login(loginBody.getUsername(), loginBody.getPassword(), loginBody.getCode(),
+                loginBody.getUuid());
+        ajax.put(Constants.TOKEN, token);
+        return ajax;
+    }
+
+    /**
      * 获取用户信息
      * 
      * @return 用户信息
